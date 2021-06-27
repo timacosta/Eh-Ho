b87daee1a29d70d241303a589dccfd89901c53f4
Tim Acosta <timacosta06@gmail.com>
2021-06-27 22:54:05 +0100

    added post ViewModel

--- app/src/main/java/io/keepcoding/eh_ho/posts/PostsActivity.kt
+++ app/src/main/java/io/keepcoding/eh_ho/posts/PostsActivity.kt
@@ -1,33 +1,98 @@
 package io.keepcoding.eh_ho.posts
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
 import io.keepcoding.eh_ho.databinding.ActivityPostsBinding
 import io.keepcoding.eh_ho.di.DIProvider
 import io.keepcoding.eh_ho.model.Topic
 
-class PostsActivity: AppCompatActivity() {
+class PostsActivity : AppCompatActivity() {
 
-    private val binding: ActivityPostsBinding by lazy { ActivityPostsBinding.inflate(layoutInflater)}
+    private val binding: ActivityPostsBinding by lazy { ActivityPostsBinding.inflate(layoutInflater) }
     private val postsAdapter = PostsAdapter()
-    private val vm: PostsViewModel by viewModels { DIProvider.topicsViewModelProviderFactory }
-    var topicID = 0
+    private val vm: PostsViewModel by viewModels { DIProvider.postsViewModelProviderFactory }
+    private val topic: Topic by lazy { intent.getSerializableExtra(TOPICS_KEY) as Topic }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        var intent = intent
-        topicID = intent.getIntExtra("topicID",0)
-
         binding.postDetails.apply {
             adapter = postsAdapter
-
+            DividerItemDecoration(
+                this@PostsActivity,
+                LinearLayout.VERTICAL
+            )
         }
 
+        vm.state.observe(this) {
+            when (it) {
+                is PostsViewModel.State.LoadingPosts -> {
+                    renderLoading(it)
+                    hideEmptyState()
+                }
+                is PostsViewModel.State.PostsReceived -> {
+                    postsAdapter.submitList(it.posts)
+                    hideEmptyState()
+                    hideLoading()
+                }
+                is PostsViewModel.State.NoPosts -> {
+                    hideLoading()
+                    renderEmptyState()
+                }
+            }
+        }
+        binding.swipePullToRefresh.setOnRefreshListener { vm.loadPosts(topic) }
+    }
 
+    override fun onResume() {
+        super.onResume()
+        vm.loadPosts(topic)
+    }
+
+    private fun renderEmptyState() {
+        binding.emptyResponse.isVisible = true
+    }
+
+    private fun hideEmptyState() {
+        binding.emptyResponse.isVisible = false
+    }
+
+    private fun hideLoading() {
+        binding.viewLoading.root.isVisible = false
+        binding.swipePullToRefresh.isRefreshing = false
+    }
+
+    private fun renderLoading(loadingState: PostsViewModel.State.LoadingPosts) {
+        when (loadingState) {
+            is PostsViewModel.State.LoadingPosts.Loading -> {
+                binding.viewLoading.root.isVisible = true
+            }
+            is PostsViewModel.State.LoadingPosts.LoadingWithPosts -> {
+                binding.swipePullToRefresh.isRefreshing = true
+                postsAdapter.submitList(loadingState.posts)
+            }
+        }
+    }
+
+
+
+
+    companion object {
+        const val TOPICS_KEY = "TOPICS_KEY"
+
+        @JvmStatic
+        fun createIntent(context: Context, topic: Topic): Intent =
+            Intent(context, PostsActivity::class.java).apply {
+                putExtra(TOPICS_KEY, topic)
+            }
     }
 
 }
--- app/src/main/java/io/keepcoding/eh_ho/posts/PostsViewModel.kt
+++ app/src/main/java/io/keepcoding/eh_ho/posts/PostsViewModel.kt
@@ -1,11 +1,62 @@
 package io.keepcoding.eh_ho.posts
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import io.keepcoding.eh_ho.model.Post
+import io.keepcoding.eh_ho.model.Topic
+import io.keepcoding.eh_ho.posts.PostsViewModel.State.PostsReceived
 import io.keepcoding.eh_ho.repository.Repository
 import io.keepcoding.eh_ho.topics.TopicsViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PostsViewModel(private val repository: Repository): ViewModel() {
+
+    private val _state: MutableLiveData<State> = MutableLiveData<State>().apply {
+        postValue(State.LoadingPosts.Loading)
+    }
+
+    val loading : MutableLiveData<Boolean> = MutableLiveData(false)
+
+    val state: LiveData<State> = _state
+
+    fun loadPosts(topic: Topic) {
+        loading.postValue(true)
+
+        _state.postValue(
+            _state.value.let {
+                when(it) {
+                    is State.PostsReceived -> State.LoadingPosts.LoadingWithPosts(it.posts)
+                    is State.LoadingPosts -> it
+                    else -> State.LoadingPosts.Loading
+                }
+            }
+        )
+        viewModelScope.launch {
+            delay(2000)
+            repository.getPosts(topic) {
+                it.fold(::onPostsReceived, ::onPostsFailure)
+            }
+        }
+    }
+
+    private fun onPostsReceived(posts: List<Post>) {
+        _state.postValue(posts.takeUnless { it.isEmpty() }?.let(::PostsReceived) ?: State.NoPosts)
+    }
+
+    private fun onPostsFailure(throwable: Throwable) {
+        _state.postValue(State.NoPosts)
+    }
+
+    sealed class State {
+        sealed class LoadingPosts : State() {
+            object Loading: LoadingPosts()
+            data class LoadingWithPosts(val posts: List<Post>) : LoadingPosts()
+        }
+        data class PostsReceived(val posts: List<Post>): State()
+        object NoPosts : State()
+    }
+
+
 
     class PostsViewModelProviderFactory(private val repository: Repository) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T = when (modelClass) {
