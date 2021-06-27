b6a0d36c9f4f37f7c47f3b0fcfbfc3ee54ba1c2a
Tim Acosta <timacosta06@gmail.com>
2021-06-27 18:05:49 +0100

    working on posts views

--- app/src/main/java/io/keepcoding/eh_ho/posts/PostsActivity.kt
+++ app/src/main/java/io/keepcoding/eh_ho/posts/PostsActivity.kt
@@ -1,5 +1,7 @@
 package io.keepcoding.eh_ho.posts
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import io.keepcoding.eh_ho.databinding.ActivityPostsBinding
@@ -10,5 +12,13 @@ class PostsActivity: AppCompatActivity() {
     private val binding: ActivityPostsBinding by lazy { ActivityPostsBinding.inflate(layoutInflater)}
     private val postsAdapter = PostsAdapter()
     private val vm: PostsViewModel by viewModels { DIProvider.topicsViewModelProviderFactory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        var intent = intent
+
+    }
 
 }
